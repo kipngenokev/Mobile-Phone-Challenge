17c04a35521023a8cd6e973b411fd8aa50602de9
brian070069 <kkipngeno409@gmail.com>
2025-02-05 23:39:42 +0300

    first commit

--- src/MobilePhone.java
+++ src/MobilePhone.java
@@ -1,6 +1,23 @@
 import java.util.ArrayList;
 
 public class MobilePhone {
-    ArrayList<String> myNumber= new ArrayList<>();
-    ArrayList<Contact> myContacts= new ArrayList<Contact>();
+    public static void main(String[] args){
+        private String myNumber;
+
+        ArrayList<Contact> myContacts = new ArrayList<>();
+
+    }
+
+    public MobilePhone(String myNumber) {
+        this.mynumber = myNumber;
+    }
+
+    public static void boolean addNewContact(Contact contact) {
+        if(myContacts.contains(contact)){
+            return true;
+        }
+        return false;
+    }
+
+    public static void boolean updateContact()
 }
